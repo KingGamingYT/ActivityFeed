6a29a0bc045318cfbdf47a0e4a00806ca4b04688
KingGamingYT <smartlevijakob@gmail.com>
2026-01-27 21:13:26 -0700

    yeehaw tuesday

--- src/activity_feed/components/now_playing/activities/components/cardActivityWrapper.tsx
+++ src/activity_feed/components/now_playing/activities/components/cardActivityWrapper.tsx
@@ -1,3 +1,23 @@
+interface CardWrapper {
+    user: Object,
+    activities: Object,
+    voice?: Object,
+    streams?: Object,
+    check: Object,
+    v2Enabled: boolean
+}
+
+export function ActivityCardWrapper({user, activities, voice, streams, check, v2Enabled}: CardWrapper) {
+    if (!activities) return;
+
+    return activities.map(activity => {
+        const currentActivity = activity?.activity || streams[0].activity;
+        const currentGame = activity?.game || GameStore.getGameByName(streams[0].activity.name);
+        const players = activity.playingMembers;
+        const server = voice[0]?.guild;
+    })
+}
+
 function ActivityCardWrapper({user, activities, voice, streams, check, v2Enabled}) {
     //console.log(size)
 
--- src/activity_feed/components/now_playing/activities/components/common/CardTrailing.tsx
+++ src/activity_feed/components/now_playing/activities/components/common/CardTrailing.tsx
@@ -3,9 +3,12 @@ import { Common } from "@modules/common";
 import MainClasses from "@activity_feed/ActivityFeed.module.css";
 import NowPlayingClasses from "@now_playing/NowPlaying.module.css";
 
-interface RichButtons {
+interface RegularButtons {
     activity: Object,
     user: Object,
+    server?: Object,
+    players?: Object,
+    check: Object,
     v2Enabled?: boolean
 }
 
@@ -16,11 +19,24 @@ interface RichButtons {
     v2Enabled?: boolean
 }
 
-export function RegularCardButtons({activity, user, players, v2Enabled}: Buttons) {
+export function RegularCardTrailing({activity, user, server, players, check, v2Enabled}: RegularButtons) {
     const [width, height] = useWindowSize();
+
+    if (width < 1240) return;
+
     return (
         <>
-            {width > 1240 && !activity?.name.includes("YouTube Music") && <div 
+            {server && <VoiceList
+                className={`${NowPlayingClasses.userList}`}
+                users={players}
+                maxUsers={players.length}
+                guildId={server?.id}
+                size="SIZE_32"
+            />}
+            {check.spotify !== 0 && <div className={`${NowPlayingClasses.serviceButtonWrapper}`}>
+                <SpotifyButtons user={user} activity={activity} />
+            </div>}
+            {!activity?.name.includes("YouTube Music") && activity?.assets ? null : <div 
                 className={`${MainClasses.button} ${NowPlayingClasses.actionsActivity} ${Common.ButtonVoidClasses.lookFilled} ${Common.PositionClasses.flex} ${Common.PositionClasses.noWrap} ${Common.PositionClasses.justifyStart}`}
                 style={{ flex: "0 1 auto", flexDirection: "column", alignItems: "flex-end", marginLeft: "20px" }}>
                 {v2Enabled && activity?.party && activity?.party?.size ? null : <Common.ActivityButtons user={user} activity={activity} />}
@@ -29,7 +45,7 @@ export function RegularCardButtons({activity, user, players, v2Enabled}: Buttons
     )
 }
 
-export function RichCardButtons({activity, user, v2Enabled}: RichButtons) {
+export function RichCardTrailing({activity, user, v2Enabled}: RichButtons) {
     const [width, height] = useWindowSize();
     return (
         <>
